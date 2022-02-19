5f1325a4d4fdc4ab339c4f93e059166a7e3f781f
tayssir <tayssirbenothmen@fsb.u-carthage.tn>
2022-02-19 20:15:27 +0100

    first commit

---
+++ UI/employeemanagerapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { EmployeeService } from './employee.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
